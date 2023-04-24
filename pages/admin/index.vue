c60bd285f07a0b11f412accffeac2129581e4c0c
zhaogongchengsi <zzh1586169624@163.com>
2023-04-24 16:46:40 +0800

    优化提交样式

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,40 +1,43 @@
 <template>
 	<div class="flex flex-col items-center">
 		<div class="flex items-center">
-			<span class="icon md:icon mr-8 i-tabler-pencil-plus"></span>
+			<span class="w-10 h-10 md:w-14 h-14 mr-8 i-tabler-pencil-plus"></span>
 			<h1 class="text-7 md:text-10 font-600 text-green-800"> Create Blog</h1>
 		</div>
 		<div class="w-full m-10  md:p-5 flex flex-col gap-6 md:gap-8">
-			<div class="icon i-tabler-h-1" />
+
 			<div
 				class="rounded-2xl p-2 md:p-5 box-border border flex items-center text-3 md:text-6 focus-within:border-red-600">
-				<label class="mr-3  w-20 md:w-35">Title</label>
+				<label class="mr-3  flex items-center justify-center w-20 md:w-35">
+					<div class="icon i-tabler-h-1" />
+				</label>
 				<input v-model="issueInfo.title" class="flex-1 bg-transparent appearance-none outline-none" type="text"
 					placeholder="Please enter a title">
 			</div>
 
-			<div class="icon i-tabler-brand-github" />
-
 			<div
 				class="rounded-2xl p-2 md:p-5 box-border border flex items-center text-3 md:text-6 focus-within:border-red-600">
-				<label class="mr-3  w-20 md:w-35">Git Hub</label>
+				<label class="mr-3 flex items-center justify-center  w-20 md:w-35">
+					<div class="icon i-tabler-brand-github" />
+				</label>
 				<input v-model="issueInfo.githubAddress" class="flex-1 bg-transparent appearance-none outline-none"
 					type="text" placeholder="Please enter the github repository address">
 			</div>
 
-			<div class="icon i-tabler-key" />
-
 			<div
-				class="rounded-2xl p-2 md:p-5 box-border border flex items-center text-3 md:text-6 focus-within:border-red-600">
-				<label class="mr-3  w-20 md:w-35">Password</label>
+				class="rounded-2xl p-3 md:p-5 box-border border flex items-center text-3 md:text-6 focus-within:border-red-600">
+				<label class="mr-3  flex items-center justify-center  w-20 md:w-35">
+					<div class="icon i-tabler-key" />
+				</label>
 				<input v-model="issueInfo.password" class="flex-1 bg-transparent appearance-none outline-none"
 					type="password" placeholder="Please enter issue token">
 			</div>
 
 			<button :disabled="isOpen"
-				class="w-full h-10 md:h-18 text-3 bg-red-600 rounded-2xl md:text-6 hover:shadow hover:md:shadow-md text-white outline- flex  justify-center items-center"
+				class="w-full h-14 md:h-18 text-3 bg-red-600 rounded-2xl md:text-6 hover:shadow hover:md:shadow-md text-white outline- flex  justify-center items-center"
 				@click="submit">
-				<div :class="[isLoading ? 'i-tabler-loader rotate' : 'i-tabler-cloud-upload', 'mr-8 icon text-white']" />
+				<div
+					:class="[isLoading ? 'i-tabler-loader rotate' : 'i-tabler-cloud-upload', 'mr-8  w-6 h-6  md:w-8 md:h-8 text-white']" />
 				Submit
 			</button>
 		</div>
